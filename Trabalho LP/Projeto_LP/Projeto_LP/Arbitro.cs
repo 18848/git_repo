9c06adff64d565d674fdb50f4233acca41951ce7
18848 <a18848@alunos.ipca.pt>
2020-04-20 22:38:26 +0100

    trabalho lp

---
+++ Trabalho LP/Projeto_LP/Projeto_LP/Arbitro.cs
@@ -0,0 +1,67 @@
+﻿/*
+*	<copyright file="Projeto_LP.cs" company="IPCA">
+*		Copyright (c) 2020 All Rights Reserved
+*	</copyright>
+* 	<author>Andre</author>
+*   <date>4/20/2020 10:08:16 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace Projeto_LP
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: Andre
+    /// Created on: 4/20/2020 10:08:16 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Arbitro
+    {
+        #region Attributes
+        //probably a better way to place these variables
+        //array or class maybe
+        int gamesTotal;
+        int gamesSeason;
+        int gamesRound;
+        int yellowCards;
+        int redCards;
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Árbitro()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Árbitro()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
---
+++ Trabalho LP/Projeto_LP/Projeto_LP/Arbitros.cs
@@ -0,0 +1,66 @@
+﻿/*
+*	<copyright file="Projeto_LP.cs" company="IPCA">
+*		Copyright (c) 2020 All Rights Reserved
+*	</copyright>
+* 	<author>Andre</author>
+*   <date>4/20/2020 10:06:20 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace Projeto_LP
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: Andre
+    /// Created on: 4/20/2020 10:06:20 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class Arbitros
+    {
+        #region Attributes
+        Arbitro[] arbitro;
+        string academy;
+        string rank;
+        string league;
+        double salary;
+        static DateTime formed;
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public Arbitros()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~Arbitros()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
